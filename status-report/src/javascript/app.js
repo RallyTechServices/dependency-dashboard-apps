baea9a39f56b2d12432713de1e6b467e63832ff3
John Martin <curmudgeon+git@rallydev.com>
2016-05-24 19:57:03 -0700

    selector button, find related features

--- status-report/src/javascript/app.js
+++ status-report/src/javascript/app.js
@@ -4,7 +4,7 @@ Ext.define("TSDependencyStatusReport", {
     logger: new Rally.technicalservices.Logger(),
     defaults: { margin: 10 },
     items: [
-        {xtype:'container',itemId:'message_box',tpl:'Hello, <tpl>{_refObjectName}</tpl>'},
+        {xtype:'container',itemId:'selector_box'},
         {xtype:'container',itemId:'display_box'}
     ],
 
@@ -14,26 +14,143 @@ Ext.define("TSDependencyStatusReport", {
                         
     launch: function() {
         var me = this;
-        this.setLoading("Loading stuff...");
-
-        this.down('#message_box').update(this.getContext().getUser());
-        
-        var model_name = 'Defect',
-            field_names = ['Name','State'];
-        
-        this._loadAStoreWithAPromise(model_name, field_names).then({
-            scope: this,
-            success: function(store) {
-                this._displayGrid(store,field_names);
-            },
-            failure: function(error_message){
-                alert(error_message);
-            }
-        }).always(function() {
-            me.setLoading(false);
-        });
+        this._addPortfolioItemSelector(this.down('#selector_box'));
     },
       
+    _addPortfolioItemSelector: function(container) {
+        container.add({ 
+            xtype:'portfolioitempickerbutton',
+            layout: 'hbox',
+            listeners: {
+                scope: this,
+                itemschosen: function(picker,items) {
+                    this.PIs = items;
+                    if ( this.PIs.length > 0 ) {
+                        this._updateData();
+                    }
+                }
+            }
+        });
+    },
+    
+    _updateData: function() {
+        this.logger.log("_updateData", this.PIs);
+        
+        Deft.Chain.pipeline([
+            this._getChildFeatures,
+            this._getRelatedFeatures
+        ],this);
+    },
+    
+    _getChildFeatures: function() {
+        this.setLoading('Fetching descendant features...');
+        var deferred = Ext.create('Deft.Deferred'),
+            me = this;
+        
+        var filter_configs = Ext.Array.map(this.PIs, function(pi) {
+            return [
+                {property:'Parent.ObjectID',value:pi.get('ObjectID')},
+                {property:'Parent.Parent.ObjectID',value:pi.get('ObjectID')}
+            ];
+        });
+        
+        var filters = Rally.data.wsapi.Filter.or(Ext.Array.flatten(filter_configs));
+        var config = {
+            model: 'PortfolioItem/Feature',
+            filters: filters,
+            fetch: ['ObjectID','FormattedID','Name','Predecessors','Successors']
+        }
+        
+        this._loadWsapiRecords(config).then({
+            scope: this,
+            success: function(features) {
+                this.logger.log("First level features:", features);
+                deferred.resolve(features);
+            },
+            failure: function(msg) {
+                deferred.reject(msg);
+            }
+        }).always(function() { me.setLoading(false); });
+        
+        return deferred.promise;
+    },
+    
+    _getRelatedFeatures: function(base_features) {
+        var me = this,
+            deferred = Ext.create('Deft.Deferred');
+        this.setLoading('Fetching predecessors/successors...');
+        
+        this.baseFeaturesByOID = {};
+        var promises = [];
+        
+        Ext.Array.each(base_features, function(feature){
+            this.baseFeaturesByOID[feature.get('ObjectID')] = feature;
+            promises.push(function() { return this._getPredecessors(feature); });
+            promises.push(function() { return this._getSuccessors(feature); });
+            
+        },this);
+        
+        Deft.Chain.sequence(promises,this).then({
+            scope: this,
+            success: function(results) {
+                this.relatedFeatures = Ext.Array.flatten(results);
+                
+                this.logger.log("RETURNED:", this.relatedFeatures);
+                this.logger.log('Base Features', this.baseFeaturesByOID);
+                deferred.resolve(this.relatedFeatures);
+            },
+            failure: function(msg) {
+                deferred.reject(msg);
+            }
+        }).always(function() { me.setLoading(false); });
+        
+        return deferred.promise;
+    },
+    
+    _getPredecessors: function(feature) {
+        var deferred = Ext.create('Deft.Deferred'),
+            me = this;
+            
+        this.logger.log('Finding predecessors for', feature.get('FormattedID'));
+        if ( feature.get('Predecessors').Count === 0 ) {
+            feature.set('_predecessors', []);
+            return [];
+        }
+        
+        feature.getCollection('Predecessors').load({
+            fetch: ['FormattedID', 'ObjectID', 'Name'],
+            scope: this,
+            callback: function(records, operation, success) {
+                feature.set('_predecessors', records);
+                deferred.resolve(records);
+            }
+        });
+        
+        return deferred.promise;
+    },
+    
+    _getSuccessors: function(feature) {
+        var deferred = Ext.create('Deft.Deferred'),
+            me = this;
+            
+        this.logger.log('Finding successors for', feature.get('FormattedID'));
+        if ( feature.get('Successors').Count === 0 ) {
+            feature.set('_successors', []);
+            return [];
+        }
+        
+        feature.getCollection('Successors').load({
+            fetch: ['FormattedID', 'ObjectID', 'Name'],
+            scope: this,
+            callback: function(records, operation, success) {
+                feature.set('_successors', records);
+                deferred.resolve(records);
+            }
+        });
+        
+        return deferred.promise;
+    },
+    
     _loadWsapiRecords: function(config){
         var deferred = Ext.create('Deft.Deferred');
         var me = this;
@@ -46,27 +163,6 @@ Ext.define("TSDependencyStatusReport", {
             callback : function(records, operation, successful) {
                 if (successful){
                     deferred.resolve(records);
-                } else {
-                    me.logger.log("Failed: ", operation);
-                    deferred.reject('Problem loading: ' + operation.error.errors.join('. '));
-                }
-            }
-        });
-        return deferred.promise;
-    },
-
-    _loadAStoreWithAPromise: function(model_name, model_fields){
-        var deferred = Ext.create('Deft.Deferred');
-        var me = this;
-        this.logger.log("Starting load:",model_name,model_fields);
-          
-        Ext.create('Rally.data.wsapi.Store', {
-            model: model_name,
-            fetch: model_fields
-        }).load({
-            callback : function(records, operation, successful) {
-                if (successful){
-                    deferred.resolve(this);
                 } else {
                     me.logger.log("Failed: ", operation);
                     deferred.reject('Problem loading: ' + operation.error.errors.join('. '));
